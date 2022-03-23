01fdc2c6327a2f8b45c150ad77b74b11c993fbc8
Katelyn <katelynxgraham@gmail.com>
2022-03-23 12:15:04 +0000

    fix bug

--- src/components/add.js
+++ src/components/add.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-class Add extends Component {
+export class Add extends Component {
     constructor() {
         super();
         //Bind to Functions to Add Movie(s)
