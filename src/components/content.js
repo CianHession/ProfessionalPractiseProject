864b0a1decbf731247d1a9ec9116c54696d2240a
Cian Hession <g00379799@gmit.ie>
2022-04-05 22:57:57 +0100

    Removed non functional auth code added before JWT

--- src/components/content.js
+++
@@ -1,6 +0,0 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import {LOGIN_TOKEN_NAME, API_ROOT, LOGIN_TOKEN_ID} from "../constants";
-
-const user = localStorage.getItem(LOGIN_TOKEN_NAME);
-const userId = localStorage.getItem(LOGIN_TOKEN_ID);
